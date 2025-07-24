8715a21857933a10d77588bef1ad325c027b0a17
laxman09751 <87252663+laxman09751@users.noreply.github.com>
2025-07-24 23:01:53 +0530

    Updated TestRunner.java with new test2 logic

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/Features/login.feature",
+		features = "src/test/resources/Features/Logout.feature",
 		glue={"stepDefinition","Hooks"},
 		 plugin = {"pretty", "html:src/test/resources/target/CucumberReports/CucumberReport.html"}
 		
